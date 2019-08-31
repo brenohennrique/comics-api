7ef608c3eb28eb6532a5007e836bf6da27d14b56
Breno Henrique <hennrique.breno@gmail.com>
2019-08-31 16:02:35 -0300

    refactor(Models/Brand): Clean code

--- app/Controllers/Http/BrandController.js
+++ app/Controllers/Http/BrandController.js
@@ -4,16 +4,10 @@ const Brand = use('App/Models/Brand')
 const Redis = use('Redis')
 
 class BrandController {
-  /**
-   * Show a list of all brands.
-   * GET brands
-   */
   async index ({ transform }) {
     const cachedBrands = await Redis.get('brands')
 
-    if (cachedBrands) {
-      return JSON.parse(cachedBrands)
-    }
+      if (cachedBrands) return JSON.parse(cachedBrands)
 
     const brands = await Brand.all()
     const brandsTransform = await transform.collection(brands, 'BrandTransformer')
@@ -24,10 +18,6 @@ class BrandController {
     return brandsTransform
   }
 
-  /**
-   * Create/save a new brand.
-   * POST brands
-   */
   async store ({ request }) {
     const data = request.only(['name'])
 
@@ -36,20 +26,12 @@ class BrandController {
     return brand
   }
 
-  /**
-   * Display a single brand.
-   * GET brands/:id
-   */
   async show ({ params }) {
     const brand = await Brand.find(params.id)
 
     return brand
   }
 
-  /**
-   * Update brand details.
-   * PUT or PATCH brands/:id
-   */
   async update ({ params, request }) {
     const data = request.only(['name'])
     const brand = await Brand.find(params.id)
@@ -61,10 +43,6 @@ class BrandController {
     return brand
   }
 
-  /**
-   * Delete a brand with id.
-   * DELETE brands/:id
-   */
   async destroy ({ params }) {
     const brand = await Brand.find(params.id)
 
